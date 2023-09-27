2ac43176953ee31cb02113f094569fdd9f63901e
Firdaws <firdaws.ab.h@gmail.com>
2023-09-27 12:03:20 +0300

    basic oparation of math

---
+++ Basic_Operation.py
@@ -0,0 +1,56 @@
+
+# understand basic oparation in math 
+
+
+
+a=33                         #multiply
+b=3
+print("a * b :",a*b)         
+print("a ** a :",a**b)       #a^b 
+print("a / b :",a/b)         #divid
+print("a // b :",a//b)    
+
+
+a=2
+b=3
+z=a+b
+print("z : ",z)              #sum
+
+
+a=6
+b=1
+print("a - b : ",a-b)        #sub
+
+
+a=7
+b=3
+print("a % b : ",a%b)           #7%3=1       : (1)(2) 3 (1)(2) 6  (1)(2) 7  -->  3 6 7 9  
+print("b % a : ",b%a)           #3%7          left side <right side   answer is left side  
+print("b % b : ",b%b)           #mode
+ 
+
+
+a=2
+b=3
+b+=a     #b=b+a
+print("b+=a :",b)
+
+
+
+
+a=3
+b=3
+print(~a)         #-4       # binary number (a=011) : ~011 = 100 
+print("~a+b : ",~a+b)        #-4+2
+
+
+print("a | b : ",a|b)            #binaty number( a =011  , b=010 )  : 011 | 010 = 011  it means 3      #  in | only (0|0=0)
+print ("a & b : ", a&b)           #binaty number( a =011  , b=010 )  : 011 & 010 = 010  it means 2      #  in & only (1&1=1)
+
+
+
+
+
+
+
+  
